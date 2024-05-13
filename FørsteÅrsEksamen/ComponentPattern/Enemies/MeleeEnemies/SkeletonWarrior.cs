4c8318add38881155ef67e4bb4b7088b425baa5b
Zanzicki <144442443+Zanzicki@users.noreply.github.com>
2024-05-13 11:40:30 +0100

    Modified Enemy

--- FørsteÅrsEksamen/ComponentPattern/Enemies/MeleeEnemies/SkeletonWarrior.cs
+++ FørsteÅrsEksamen/ComponentPattern/Enemies/MeleeEnemies/SkeletonWarrior.cs
@@ -1,10 +1,11 @@
-﻿using FørsteÅrsEksamen.GameManagement;
+﻿using FørsteÅrsEksamen.ComponentPattern.Enemies.Skeleton;
+using FørsteÅrsEksamen.GameManagement;
 
-namespace FørsteÅrsEksamen.ComponentPattern.Enemies.Skeleton
+namespace FørsteÅrsEksamen.ComponentPattern.Enemies.MeleeEnemies
 {
     //Asser
 
-    internal class SkeletonWarrior : Skeleton
+    internal class SkeletonWarrior : Melee
     {
         public SkeletonWarrior(GameObject gameObject) : base(gameObject)
         {
@@ -17,6 +18,11 @@ namespace FørsteÅrsEksamen.ComponentPattern.Enemies.Skeleton
             characterStateAnimations.Add(CharacterState.Idle, AnimNames.OrcIdle);
             characterStateAnimations.Add(CharacterState.Moving, AnimNames.OrcRun);
             characterStateAnimations.Add(CharacterState.Dead, AnimNames.OrcDeath);
+        }
+
+        internal override void AttackAction()
+        {
+
         }
 
     }
