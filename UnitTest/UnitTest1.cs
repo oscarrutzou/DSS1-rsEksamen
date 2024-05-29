dda2364a16bc5430081ddf5e5e1904c73890a641
oscarrutzou <oscargamer999@gmail.com>
2024-05-29 18:33:43 +0200

    Unit test

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+using DoctorsDungeon.Other;
+using Microsoft.Xna.Framework;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void RotateTest()
+        {
+            Vector2 startPos = new(0, 1);
+            float rotation = -MathHelper.PiOver2;
+
+            Vector2 expectedPos = new(1, 0);
+            Vector2 resultPos = BaseMath.Rotate(startPos, rotation);
+            Assert.IsTrue(Vector2.Distance(expectedPos, resultPos) < 1e-5);
+        }
+    }
+}
